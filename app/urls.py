64d6866b46e796c639985bca096a45946865b133
ulpa11 <ashutoshraghuwanshi28@gmail.com>
2023-02-08 18:46:18 +0530

    last commit

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import main, treatment_page, treatment_complete
+from .views import main, treatment_page, treatment_complete, add_wifi
 
 urlpatterns = [
     path('', main, name='main'),
     path('treatment/', treatment_page, name='treatment'),
     path('treatment_complete/', treatment_complete, name='treatment_complete'),
+    path('add_wifi/', add_wifi, name='add_wifi'),
+    
 ]
--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ import requests
 import json
 from .tubeA import process_tubeA
 from .tubeB import process_tubeB
+import subprocess
 
 
 
@@ -32,6 +33,9 @@ def main(request):
             print("start")
             #redirect to treatment page
             return redirect("/treatment/")
+        elif request.POST.get("add-wifi-button")=="add-wifi-button":
+            #redirect to add wifi page
+            return redirect("/add_wifi/")
     return render(request, 'main.html', data)
 
 def treatment_page(request):
@@ -82,10 +86,35 @@ def treatment_complete(request):
     return render(request, 'treatment_complete.html')
 
 
+def add_wifi(request):
+    if request.method=="POST":
+        #call wifi name 
+        ssid= request.POST.get("wifi_name")
+        #call wifi password
+        password = request.POST.get("password")
+        wpa_supplicant_conf = f"""
+                country=US
+                ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+                update_config=1
+                network={{
+                ssid="{ssid}"
+                psk="{password}"
+                key_mgmt=WPA-PSK
+                }}
+        """
+        try:
+            with open("/etc/wpa_supplicant/wpa_supplicant.conf", "w") as f:
+                f.write(wpa_supplicant_conf)
+            subprocess.call(["wpa_cli", "-i", "wlan0", "reconfigure"])
+            subprocess.call(["dhclient", "wlan0"])
+            return redirect("/")
+        except:
+            #add message warning that failed to connect to wifi
+            messages.warning(request, "Failed to connect to wifi")
+            return redirect("/")
 
 
-
-
+    return render(request, 'login.html')
 
 
 
