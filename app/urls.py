7d91788d483e8a547f18e5631de4211192cbe2d4
ulpa11 <ashutoshraghuwanshi28@gmail.com>
2023-02-09 14:35:39 +0530

    last commit

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,5 @@
 from django.urls import path
-from .views import main, treatment_page, treatment_complete, add_wifi
-
+from . import views
 urlpatterns = [
-    path('', main, name='main'),
-    path('treatment/', treatment_page, name='treatment'),
-    path('treatment_complete/', treatment_complete, name='treatment_complete'),
-    path('add_wifi/', add_wifi, name='add_wifi'),
-    
+    path('',views.main,name='main'),
 ]
